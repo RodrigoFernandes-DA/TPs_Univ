727dd99c8fec8b0b43dfaf33cc2e29e47c80412b
Rodrigo Fernandes <fernandes.rcc@gmail.com>
2025-10-17 17:32:32 +0200

    Dev TP1&2 AIP

--- AIP/functionsTP.py
+++ AIP/functionsTP.py
@@ -4,7 +4,10 @@ from matplotlib import pyplot as plt
 from skimage.metrics import mean_squared_error, peak_signal_noise_ratio, structural_similarity
 from skimage.io import imread
 import scipy.ndimage as ndimage
+# import skimage.filter as filt
 
+
+######## PART 1 - IMAGE DENOISING ###############
 
 def bilateral_filter_scratch(src, d, sigma_color, sigma_space):
     radius = d // 2
@@ -68,7 +71,6 @@ def isotropic_diffusion(image, num_iterations=100, eta=0.1):
     return u
 
 
-import numpy as np
 
 def anisotropic_diffusion(image, num_iterations=15, eta=0.25, K=15):
     """
@@ -109,3 +111,149 @@ def anisotropic_diffusion(image, num_iterations=15, eta=0.25, K=15):
         u += eta * (cN * dN + cS * dS + cE * dE + cW * dW)
     
     return u
+
+
+######## PART 2 - IMAGE SEGMENTATION ###############
+
+
+def create_A(a, b, N):
+    """
+    a: float
+    alpha parameter
+
+    b: float
+    beta parameter
+
+    N: int
+    N is the number of points sampled on the snake curve: (x(p_i), y(p_i)), i=0,...,N-1
+    """
+    row = np.r_[
+        -2*a - 6*b, 
+        a + 4*b,
+        -b,
+        np.zeros(N-5),
+        -b,
+        a + 4*b
+    ]
+    A = np.zeros((N,N))
+    for i in range(N):
+        A[i] = np.roll(row, i)
+    return A
+
+def create_external_edge_force_gradients_from_img( img, sigma=30. ):
+    """
+    Given an image, returns 2 functions, fx & fy, that compute
+    the gradient of the external edge force in the x and y directions.
+
+    img: ndarray
+        The image.
+    """
+    # Gaussian smoothing.
+    # smoothed = filt.gaussian_filter( (img-img.min()) / (img.max()-img.min()), sigma )
+    smoothed = cv2.GaussianBlur((img-img.min()) / (img.max()-img.min()), (5,5), sigma=10.0)
+    # Gradient of the image in x and y directions.
+    giy, gix = np.gradient( smoothed )
+    # Gradient magnitude of the image.
+    gmi = (gix**2 + giy**2)**(0.5)
+    # Normalize. This is crucial (empirical observation).
+    gmi = (gmi - gmi.min()) / (gmi.max() - gmi.min())
+
+    # Gradient of gradient magnitude of the image in x and y directions.
+    ggmiy, ggmix = np.gradient( gmi )
+
+    def fx(x, y):
+        """
+        Return external edge force in the x direction.
+
+        x: ndarray
+            numpy array of floats.
+        y: ndarray:
+            numpy array of floats.
+        """
+        # Check bounds.
+        x[ x < 0 ] = 0.
+        y[ y < 0 ] = 0.
+
+        x[ x > img.shape[1]-1 ] = img.shape[1]-1
+        y[ y > img.shape[0]-1 ] = img.shape[0]-1
+
+        return ggmix[ (y.round().astype(int), x.round().astype(int)) ]
+
+    def fy(x, y):
+        """
+        Return external edge force in the y direction.
+
+        x: ndarray
+            numpy array of floats.
+        y: ndarray:
+            numpy array of floats.
+        """
+        # Check bounds.
+        x[ x < 0 ] = 0.
+        y[ y < 0 ] = 0.
+
+        x[ x > img.shape[1]-1 ] = img.shape[1]-1
+        y[ y > img.shape[0]-1 ] = img.shape[0]-1
+
+        return ggmiy[ (y.round().astype(int), x.round().astype(int)) ]
+
+    return fx, fy
+
+def iterate_snake(x, y, a, b, fx, fy, gamma=0.1, n_iters=10, return_all=True):
+    """
+    x: ndarray
+        intial x coordinates of the snake
+
+    y: ndarray
+        initial y coordinates of the snake
+
+    a: float
+        alpha parameter
+
+    b: float
+        beta parameter
+
+    fx: callable
+        partial derivative of first coordinate of external energy function. This is the first element of the gradient of the external energy.
+
+    fy: callable
+        see fx.
+
+    gamma: float
+        step size of the iteration
+    
+    n_iters: int
+        number of times to iterate the snake
+
+    return_all: bool
+        if True, a list of (x,y) coords are returned corresponding to each iteration.
+        if False, the (x,y) coords of the last iteration are returned.
+    """
+    A = create_A(a,b,x.shape[0])
+    B = np.linalg.inv(np.eye(x.shape[0]) - gamma*A)
+    if return_all:
+        snakes = []
+
+    for i in range(n_iters):
+        x_ = np.dot(B, x + gamma*fx(x,y))
+        y_ = np.dot(B, y + gamma*fy(x,y))
+        x, y = x_.copy(), y_.copy()
+        if return_all:
+            snakes.append( (x_.copy(),y_.copy()) )
+
+    if return_all:
+        return snakes
+    else:
+        return (x,y)
+    
+
+if __name__ == "__main__":
+    image_path = 'Image2.jpg'
+    image = cv2.imread(image_path)
+    # image = cv2.cvtColor(L_image, cv2.COLOR_BGR2GRAY)
+
+    smoothed = cv2.GaussianBlur((image-image.min()) / (image.max()-image.min()), (5,5), sigma=10.0)
+
+    plt.imshow(smoothed, cmap='gray')
+    plt.title('Original Image')
+
